809683d2181b9bcf54e7ab6fb6460dc4b86fcd69
awesomedays <l50122498@gmail.com>
2024-03-13 17:00:27 +0900

    Repository Published

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 moveDir = Vector2.zero;
+    Vector2 _moveDir;
+    float _speed = 5.0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        UpdateInput();
+        MovePlayer();
+    }
+    
+    void UpdateInput()
+    {
+        Vector2 moveDir = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+            moveDir.y += 1;
+        if (Input.GetKey(KeyCode.S))
+            moveDir.y -= 1;
+        if (Input.GetKey(KeyCode.A))
+            moveDir.x -= 1;
+        if (Input.GetKey(KeyCode.D))
+            moveDir.x += 1;
+
+        _moveDir = moveDir.normalized;
+    }
+
+    void MovePlayer()
+    {
+        Vector3 dir = _moveDir * _speed * Time.deltaTime;
+        transform.position += dir;
+    }
+}
